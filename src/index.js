a79e605769cab4ef929c5b020d0ebd9b61a58407
ZippyDaFish <andrew.riker21@gmail.com>
2022-11-06 21:46:56 -0500

    Commenting for clarification

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 console.log("Everything seems to be working");
 
+//temp function to ensure event listeners are working
 function logButton(){
     console.log("UwU");
 }
 
+// event listeners for dashboard tabs
 document.getElementById('inbox-btn').addEventListener('click', logButton);
 document.getElementById('today-btn').addEventListener('click', logButton);
 document.getElementById('week-btn').addEventListener('click', logButton);
-
+// event listener for adding a new project
 document.getElementById('project-add-btn').addEventListener('click', logButton);
