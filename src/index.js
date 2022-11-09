8089e96f29d284872bccdfc5f529b8f84795509a
ZippyDaFish <andrew.riker21@gmail.com>
2022-11-09 17:01:24 -0500

    Comments for clarification

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+let inbox = [];
 let projects = [];
 let todos = [];
 
+// Handles creation of content and links to storage of content
 const contentCreation = (() => {
     const createProject = () => {
         //Todo
@@ -8,11 +10,14 @@ const contentCreation = (() => {
     };
     const createTodo = () => {
         //Todo
+        //Todo creation can be linked to whatever tab user is currently on
+        //Add todo button will no be displayed within Today and This Week tabs
         console.log("New Todo");
     };
     return{createProject, createTodo};
 })();
 
+// Handles displaying content onto the webpage
 const contentDisplay = (() => {
     const displayInbox = () => {
         //Todo
