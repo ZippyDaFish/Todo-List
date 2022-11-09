3b39a0e674af5473b54a2eb83f0128767cd0193b
ZippyDaFish <andrew.riker21@gmail.com>
2022-11-09 16:50:56 -0500

    Add display functions and connect them to eventListeners

--- src/index.js
+++ src/index.js
@@ -16,14 +16,23 @@ const contentCreation = (() => {
 const contentDisplay = (() => {
     const displayInbox = () => {
         //Todo
+        console.log("Nav to Inbox tab");
     };
-    return{displayInbox};
+    const displayToday = () => {
+        //Todo
+        console.log("Nav to Today tab");
+    }
+    const displayWeekly = () => {
+        //Todo
+        console.log("Nav to Weekly tab");
+    }
+    return{displayInbox, displayToday, displayWeekly};
 })();
 
 // event listeners for dashboard tabs
-document.getElementById('inbox-btn').addEventListener('click', console.log("Meep"));
-document.getElementById('today-btn').addEventListener('click', console.log("Meep"));
-document.getElementById('week-btn').addEventListener('click', console.log("Meep"));
+document.getElementById('inbox-btn').addEventListener('click', contentDisplay.displayInbox);
+document.getElementById('today-btn').addEventListener('click', contentDisplay.displayToday);
+document.getElementById('week-btn').addEventListener('click', contentDisplay.displayWeekly);
 // event listener for adding a new project and a new todo
 document.getElementById('project-add-btn').addEventListener('click', contentCreation.createProject);
 document.getElementById('todo-add-btn').addEventListener('click', contentCreation.createTodo);
