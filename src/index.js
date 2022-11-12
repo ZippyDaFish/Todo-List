adcd3e07dd4cd94bdbaff379ab1ebf008a66924d
ZippyDaFish <andrew.riker21@gmail.com>
2022-11-12 13:33:09 -0500

    Add project nav event listener

--- src/index.js
+++ src/index.js
@@ -34,14 +34,16 @@ const contentDisplay = (() => {
     const displayProject = (project) => {
         //Todo
         //Navs to given project and displays all Todos within
+        console.log("Nav to " + project);
     };
-    return{displayInbox, displayToday, displayWeekly};
+    return{displayInbox, displayToday, displayWeekly, displayProject};
 })();
 
 // event listeners for dashboard tabs
 document.getElementById('inbox-btn').addEventListener('click', contentDisplay.displayInbox);
 document.getElementById('today-btn').addEventListener('click', contentDisplay.displayToday);
 document.getElementById('week-btn').addEventListener('click', contentDisplay.displayWeekly);
+document.getElementById('0-project-btn').addEventListener('click', function(){ contentDisplay.displayProject(0)});
 // event listener for adding a new project and a new todo
 document.getElementById('project-add-btn').addEventListener('click', contentCreation.createProject);
 document.getElementById('todo-add-btn').addEventListener('click', contentCreation.createTodo);
